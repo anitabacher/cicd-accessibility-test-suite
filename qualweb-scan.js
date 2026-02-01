94d73d4b7ee54835de5258d63dbb7f88a1a09573
Bacher Anita <anita.bacher@edu.fh-joanneum.at>
2026-02-01 21:23:27 +0100

    trying differnt places for sandbox fix

--- qualweb-scan.js
+++ qualweb-scan.js
@@ -1,49 +1,57 @@
 const { QualWeb } = require('@qualweb/core');
 const fs = require('fs');
 
-// 1. Get Url
 const targetUrl = process.argv[2] || 'http://localhost:1338/';
 
 (async () => {
     try {
         const qualweb = new QualWeb({});
 
-        // 2. Start Browser
-        // using 'puppeteerOptions', due to problems with Chrome
-        await qualweb.start({
-            maxConcurrency: 1,
+        // all arguments to force Chrome to start without sandbox
+        const browserArgs = [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu'
+        ];
+
+        // Trying the arguments in different places
+        const startOptions = {
+            headless: true,
+            args: browserArgs, // Try 1: in Root
+            puppeteer: {
+                headless: true,
+                args: browserArgs // Try 2: In 'puppeteer' Object
+            },
             puppeteerOptions: {
-                headless: 'new', // using headless mode in chrome
-                args: [
-                    '--no-sandbox',
-                    '--disable-setuid-sandbox',
-                    '--disable-dev-shm-usage', // important for CI
-                    '--disable-gpu'
-                ]
+                headless: true,
+                args: browserArgs // Try 3: In 'puppeteerOptions' Objects
+            },
+            launchOptions: {
+                args: browserArgs // Try 4: I 'launchOptions' Object
             }
-        });
+        };
+
+        await qualweb.start(startOptions);
 
         console.log(`🚀 Scanning: ${targetUrl}`);
 
-        // Configuration for scan
         const options = {
             url: targetUrl,
             execute: {
-                act: true,    // ACT Rules
-                wcag: true,   // WCAG Rules
-                bp: false     // Best Practices
+                act: true,
+                wcag: true,
+                bp: false
             }
         };
 
         const report = await qualweb.evaluate(options);
-
-        // 3. Save Results
         fs.writeFileSync('report.json', JSON.stringify(report, null, 2));
 
         await qualweb.stop();
 
     } catch (error) {
-        console.error("QUALWEB ERROR:", error);
+        console.error("QUALWEB CRASH:", error);
         process.exit(1);
     }
 })();
